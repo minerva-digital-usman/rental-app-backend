856b75d250bfc59e535f540554b1d0f227db1932
Zeeshan <zeeshan6910@gmail.com>
2025-05-02 10:52:40 +0200

    debug

--- middleware_platform/settings.py
+++ middleware_platform/settings.py
@@ -146,16 +146,27 @@ WSGI_APPLICATION = 'middleware_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'project',  # Your PostgreSQL database name
-        'USER': 'postgres',  # Your PostgreSQL user
-        'PASSWORD': '221299',  # Your PostgreSQL user's password
-        'HOST': 'localhost',  # If PostgreSQL is running locally
-        'PORT': '5432',  # Default PostgreSQL port
+
+
+if DEBUG:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "project",
+            "USER": "postgres",
+            "PASSWORD": "221299",
+            "HOST": "localhost",
+            "PORT": "5432",
+        }
+    }
+
 
 
 # Password validation
